ff65505f7a4990de602e30048d4c81df5b89a153
Federico <federicoscutti@gmail.com>
2018-09-21 02:10:29 +0000

    remove probe vars

--- ssdilep/algs/vars.py
+++ ssdilep/algs/vars.py
@@ -148,11 +148,11 @@ class BuildTrigConfig(pyframe.core.Algorithm):
       mu_trigger_slice["HLT_mu50"]  = (43. * GeV,  nolim * GeV)
 
 
-      if self.key == "taus": self.store["ChainsWithTau"] = {} 
-      
-      for i,trig in enumerate(self.chain.tau_listTrigChains):
-        if trig in self.store["ChainsWithTau"].keys(): continue
-        self.store["ChainsWithTau"][trig] = i
+      if self.key == "taus": 
+        self.store["ChainsWithTau"] = {} 
+        for i,trig in enumerate(self.chain.tau_listTrigChains):
+          if trig in self.store["ChainsWithTau"].keys(): continue
+          self.store["ChainsWithTau"][trig] = i
 
 
       # initialise the different slices
@@ -171,7 +171,7 @@ class BuildTrigConfig(pyframe.core.Algorithm):
       if self.sampletype == "mc" or not self.get_prescales:
         prescale_info = self.chain.passedTriggers
       elif self.get_prescales:
-        prescale_info = self.chain.triggerPrescales
+        prescale_info = self.chain.triggerPrescalesLumi
 
 
       if not "passTrig" in self.store.keys():
@@ -220,6 +220,13 @@ class Particle(pyframe.core.ParticleProxy):
       matchtype = self.truthType in [6]
       #return self.isTruthMatchedToMuon and matchtype
       return matchtype
+    #__________________________________________________________________________
+    def isTrueHadTau(self):
+      matchtype = False
+      if hasattr(self, 'isTrueHadronicTau'):
+        matchtype = self.isTrueHadronicTau in [1]
+      return matchtype
+
 
 
 class ParticlesBuilder(pyframe.core.Algorithm):
@@ -295,123 +302,6 @@ class TagAndProbeVars(pyframe.core.Algorithm):
           self.store['tag'] = muon1
           self.store['probe'] = muon2
 
-
-        return True
-
-
-#------------------------------------------------------------------------------
-class ProbeVars(pyframe.core.Algorithm):
-    """
-    computes variables for the tag-and-probe selection
-    in the di-muon channel
-    """
-    #__________________________________________________________________________
-    def __init__(self, 
-                 name      = 'ProbeVars',
-                 key_tag   = 'tag',
-                 key_probe = 'probe',
-                 ):
-        pyframe.core.Algorithm.__init__(self, name)
-        
-        self.key_tag   = key_tag
-        self.key_probe = key_probe
-    #__________________________________________________________________________
-    def execute(self, weight):
-        pyframe.core.Algorithm.execute(self, weight)
-        """
-        computes variables and puts them in the store
-        """
-
-        ## get objects from event candidate
-        ## --------------------------------------------------
-        assert self.store.has_key(self.key_tag), "tag key: %s not found in store!" % (self.key_tag)
-        assert self.store.has_key(self.key_probe), "probe key: %s not found in store!" % (self.key_probe)
-        tag   = self.store[self.key_tag]
-        probe = self.store[self.key_probe]
-       
-        """
-        iso/pt<0.8
-        ----------
-        p0  = 5.66581   +/-   6.81268
-        p1  = 1.25722   +/-   0.0603463 
-        """
-        
-        p0  = 5.66581
-        p1  = 1.25722
-
-        # RMS is in GeV
-        RMS = {}
-
-        RMS["ptiso_20_25"]   = 0.0
-        RMS["ptiso_25_30"]   = 12.4064460713
-        RMS["ptiso_30_35"]   = 13.2433526355
-        RMS["ptiso_35_40"]   = 14.4159208352
-        RMS["ptiso_40_45"]   = 16.2752782594
-        RMS["ptiso_45_50"]   = 18.2407633765
-        RMS["ptiso_50_55"]   = 19.5622710782
-        RMS["ptiso_55_60"]   = 20.7644848946
-        RMS["ptiso_60_65"]   = 22.4369845476
-        RMS["ptiso_65_70"]   = 24.2929719957
-        RMS["ptiso_70_75"]   = 26.7638147383
-        RMS["ptiso_75_80"]   = 27.4958307282
-        RMS["ptiso_80_85"]   = 28.6467918032
-        RMS["ptiso_85_90"]   = 30.3006145851
-        RMS["ptiso_90_95"]   = 31.4212980513
-        RMS["ptiso_95_100"]  = 32.3088211841
-        RMS["ptiso_100_105"] = 33.1844260853
-        RMS["ptiso_105_110"] = 34.7326856135
-        RMS["ptiso_110_115"] = 35.2736180541
-        RMS["ptiso_115_120"] = 36.4129657978
-        RMS["ptiso_120_125"] = 37.4741421704
-        RMS["ptiso_125_130"] = 37.7874721485
-        RMS["ptiso_130_135"] = 38.557678202
-        RMS["ptiso_135_140"] = 40.4975404537
-        RMS["ptiso_140_145"] = 40.998314068
-        RMS["ptiso_145_150"] = 42.8896945092
-        RMS["ptiso_150_155"] = 43.5952011684
-        RMS["ptiso_155_160"] = 44.1735072515
-        RMS["ptiso_160_165"] = 45.5866562887
-        RMS["ptiso_165_170"] = 47.524117093
-        RMS["ptiso_170_175"] = 44.5405330826
-        RMS["ptiso_175_180"] = 48.3866372102
-        RMS["ptiso_180_185"] = 52.0070717859
-        RMS["ptiso_185_190"] = 47.744819892
-        RMS["ptiso_190_195"] = 50.5839599775
-        RMS["ptiso_195_200"] = 54.7332840655
-        RMS["ptiso_200_205"] = 53.1491008055
-        RMS["ptiso_205_210"] = 52.2524439685
-        RMS["ptiso_210_215"] = 54.2499328095
-        RMS["ptiso_215_220"] = 55.2824187879
-        RMS["ptiso_220_225"] = 57.1261788076
-        RMS["ptiso_225_230"] = 53.3562019687
-        RMS["ptiso_230_235"] = 63.8959744557
-        RMS["ptiso_235_240"] = 58.3227138415
-        RMS["ptiso_240_245"] = 62.1696165824
-        RMS["ptiso_245_250"] = 60.1443277898
-        RMS["ptiso_250_255"] = 43.3303155178
-        RMS["ptiso_255_260"] = 49.5807605407
-        RMS["ptiso_260_265"] = 63.1491729748
-        RMS["ptiso_265_270"] = 66.1689375705
-        RMS["ptiso_270_275"] = 81.6534137744
-        RMS["ptiso_275_280"] = 83.7920957755
-        RMS["ptiso_280_285"] = 45.3639772014
-        RMS["ptiso_285_290"] = 74.506656773
-        RMS["ptiso_290_295"] = 69.0819259747
-        RMS["ptiso_295_300"] = 56.9806508594
-        RMS["ptiso_300_305"] = 50.6671197602
-        RMS["ptiso_305_310"] = 71.6676212378
-        RMS["ptiso_310_315"] = 62.3832400493
-        RMS["ptiso_315_320"] = 94.7307677967
-        
-        probe_ptiso = ( probe.tlv.Pt() + probe.ptvarcone30 ) / GeV
-
-        probe_RMS = 94.7307677967 # last bin!
-        
-        for k,v in RMS.iteritems():
-          if probe_ptiso >= float(k.split("_")[1]) and probe_ptiso < float(k.split("_")[2]):
-            probe_RMS = v
-
-        self.store["probe_ujet_pt"] =  p1 * probe_ptiso + p0 + random.gauss(0.,probe_RMS)
 
         return True
 
